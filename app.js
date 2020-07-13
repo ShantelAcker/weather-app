1727254b90518ed2eea74cb762c9d9204fbaa931
ShantelAcker <56419079+shantelacker@users.noreply.github.com>
2020-07-12 21:28:31 -0500

    add icons

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 window.addEventListener('load', () => {
     let long;
     let lat;
+    let temperatureDescription = document.querySelector('.temperature-description');
+    let temperatureDegree = document.querySelector('.temperature-degree');
+    let locationTimezone = document.querySelector('.location-timezone');
+    let weatherIcon = document.querySelector('.weather-icon');
 
     if(navigator.geolocation) {
         //getting the coordinates from the user
@@ -13,7 +17,7 @@ window.addEventListener('load', () => {
 
             //our api key from open weather map
             const key = '898f442c701508af007773e6e6727c48';
-            const api = `${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${key}`;
+            const api = `${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${key}&units=imperial`; //add units imperial to get correct temp in fahrenheit
 
             fetch(api)
             .then(response => {
@@ -21,10 +25,19 @@ window.addEventListener('load', () => {
             })
             .then(data => {
                 console.log(data);
-                //a way to shorthand to avoid having to type every object name
+                //a way to shorthand to avoid having to type every object property name
                 const {temp} = data.main;
-                const {description} = data.weather;
-            })
+                const {description, icon} = data.weather[0];
+                const {name} = data;
+
+                const weatherImage = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+
+                //set DOM elements equal to api data
+                temperatureDegree.textContent = temp;
+                temperatureDescription.textContent = description; 
+                locationTimezone.textContent = name;
+                weatherIcon.innerHTML = `<img src="icons/${icon}.png">`;
+            });
         });
 
     } else {
